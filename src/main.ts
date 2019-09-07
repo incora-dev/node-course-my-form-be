032ae37b78e3e9c555fcd0c1878ef2995bc4c561
bohdanbrn <bohdanbrn@gmail.com>
2019-09-07 23:03:29 +0300

    fix CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
     const logger = new Logger('bootstrap');
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
 
     const options = new DocumentBuilder()
