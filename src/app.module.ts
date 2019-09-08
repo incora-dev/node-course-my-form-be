d034a7678a7a240e19b23eadff71f991af368445
Andrii Savka <andriisavka7@gmail.com>
2019-09-08 12:34:20 +0300

    MF-8 POST /forms endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { FormsModule } from "./forms/forms.module";
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, UsersModule],
+    imports: [
+        TypeOrmModule.forRoot(typeOrmConfig),
+        AuthModule,
+        UsersModule,
+        FormsModule,
+    ],
 })
 export class AppModule {}
