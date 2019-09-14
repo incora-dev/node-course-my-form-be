ddf9b18a159688f913236e1a222733d16d8fd5c0
bohdanbrn <bohdanbrn@gmail.com>
2019-09-15 01:31:34 +0300

    fix e2e tests for auth

--- test/seeds/users.seed.ts
+++ test/seeds/users.seed.ts
@@ -1,28 +1,57 @@
-const usersSeed = [
-    {
-        email: 'testAdmin@gmail.com',
-        password: '111111Qq',
-        firstName: 'Test',
-        lastName: 'Admin',
-        address: 'Test address',
-        country: 'Test country',
-        city: 'Test city',
-        postalCode: 'Test postalCode',
-        aboutMe: 'Test aboutMe',
-        role: 'ADMIN',
-    },
-    {
-        email: 'testUser@gmail.com',
-        password: '111111Qq',
-        firstName: 'Test',
-        lastName: 'User',
-        address: 'Test address',
-        country: 'Test country',
-        city: 'Test city',
-        postalCode: 'Test postalCode',
-        aboutMe: 'Test aboutMe',
-        role: 'USER',
-    },
-];
+import { UserRole } from '../../src/users/enums/user-role.enum';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
-export { usersSeed };
+export const usersSeed: { admins: CreateUserDto[]; users: CreateUserDto[] } = {
+    admins: [
+        {
+            email: 'testAdmin1@gmail.com',
+            password: '111111Qq',
+            firstName: 'Test',
+            lastName: 'Admin1',
+            address: 'Test address',
+            country: 'Test country',
+            city: 'Test city',
+            postalCode: 'Test postalCode',
+            aboutMe: 'Test aboutMe',
+            role: UserRole.ADMIN,
+        },
+        {
+            email: 'testAdmin2@gmail.com',
+            password: '111111Qq',
+            firstName: 'Test',
+            lastName: 'Admin2',
+            address: 'Test address',
+            country: 'Test country',
+            city: 'Test city',
+            postalCode: 'Test postalCode',
+            aboutMe: 'Test aboutMe',
+            role: UserRole.ADMIN,
+        },
+    ],
+    users: [
+        {
+            email: 'testUser1@gmail.com',
+            password: '111111Qq',
+            firstName: 'Test',
+            lastName: 'User1',
+            address: 'Test address',
+            country: 'Test country',
+            city: 'Test city',
+            postalCode: 'Test postalCode',
+            aboutMe: 'Test aboutMe',
+            role: UserRole.USER,
+        },
+        {
+            email: 'testUser2@gmail.com',
+            password: '111111Qq',
+            firstName: 'Test',
+            lastName: 'User2',
+            address: 'Test address',
+            country: 'Test country',
+            city: 'Test city',
+            postalCode: 'Test postalCode',
+            aboutMe: 'Test aboutMe',
+            role: UserRole.USER,
+        },
+    ],
+};
