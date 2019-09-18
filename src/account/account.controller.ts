344a7e01cd3128d46f26abfa831fc684c0f5d52b
bohdanbrn <bohdanbrn@gmail.com>
2019-09-19 00:07:18 +0300

    PUT /account

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -3,14 +3,24 @@ import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/users/user.entity';
 import { AccountService } from './account.service';
 import { GetUser } from 'src/users/decorators/get-user-decorator';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('account')
 @UseGuards(AuthGuard('jwt'))
 export class AccountController {
-    constructor(private accountService: AccountService) {}
+    constructor(private accountService: AccountService, private usersService: UsersService) {}
 
     @Get()
     async getAccount(@GetUser() user: User): Promise<User> {
         return user;
+    }
+
+    @Put()
+    async updateAccount(
+        @Body() updateUserDto: UpdateUserDto,
+        @GetUser() user: User,
+    ): Promise<User> {
+        return await this.usersService.updateUser(user.id, updateUserDto);
     }
 }
