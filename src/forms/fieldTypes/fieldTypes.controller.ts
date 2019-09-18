f0a04b9a7987f7bc633888054b107216663ebd84
bohdanbrn <bohdanbrn@gmail.com>
2019-09-18 23:38:54 +0300

    fix permissions for form routes

--- src/forms/fieldTypes/fieldTypes.controller.ts
+++ src/forms/fieldTypes/fieldTypes.controller.ts
@@ -1,9 +1,5 @@
 import { Controller, Post, Get, Put, Delete, Param, UseGuards, Body, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { UserRole } from 'src/users/enums/user-role.enum';
-import { RolesGuard } from 'src/guards/roles.guard';
-import { Roles } from '../../decorators/roles.decorator';
-import { GetUser } from '../../users/decorators/get-user-decorator';
 import { FieldType } from './fieldType.entity';
 import { FieldTypesService } from './fieldTypes.service';
 
@@ -13,15 +9,11 @@ export class FieldTypesController {
     constructor(private fieldTypesService: FieldTypesService) {}
 
     @Get()
-    @Roles(UserRole.ADMIN)
-    @UseGuards(RolesGuard)
     async getFieldTypes(): Promise<FieldType[]> {
         return await this.fieldTypesService.getFieldTypes();
     }
 
     @Get('/:id')
-    @Roles(UserRole.ADMIN)
-    @UseGuards(RolesGuard)
     async getFieldTypeByParams(@Param('id') id: number): Promise<FieldType> {
         return await this.fieldTypesService.getFieldTypeByParams({ id });
     }
--- src/forms/forms.controller.ts
+++ src/forms/forms.controller.ts
@@ -2,9 +2,6 @@ import { Controller, Post, Get, Put, Delete, Param, UseGuards, Body } from '@nes
 import { AuthGuard } from '@nestjs/passport';
 import { Form } from './form.entity';
 import { User } from '../users/user.entity';
-import { Roles } from '../decorators/roles.decorator';
-import { UserRole } from '../users/enums/user-role.enum';
-import { RolesGuard } from '../guards/roles.guard';
 import { GetUser } from '../users/decorators/get-user-decorator';
 import { FormsService } from './forms.service';
 import { CreateFormDto } from './dto/create-form.dto';
@@ -16,29 +13,21 @@ export class FormsController {
     constructor(private formsService: FormsService) {}
 
     @Post()
-    @Roles(UserRole.ADMIN)
-    @UseGuards(RolesGuard)
     async createForm(@Body() createFormDto: CreateFormDto, @GetUser() user: User): Promise<Form> {
         return await this.formsService.createForm(createFormDto, user);
     }
 
     @Get()
-    @Roles(UserRole.ADMIN)
-    @UseGuards(RolesGuard)
     async getUserForms(@GetUser() user: User): Promise<Form[]> {
         return await this.formsService.getUserForms(user.id);
     }
 
     @Get('/:id')
-    @Roles(UserRole.ADMIN)
-    @UseGuards(RolesGuard)
     async getUserForm(@Param('id') formId: number, @GetUser() user: User): Promise<Form> {
         return await this.formsService.getUserForm(formId, user.id);
     }
 
     @Put('/:id')
-    @Roles(UserRole.ADMIN)
-    @UseGuards(RolesGuard)
     async updateForm(
         @Param('id') formId: number,
         @Body() updateFormDto: UpdateFormDto,
@@ -48,8 +37,6 @@ export class FormsController {
     }
 
     @Delete('/:id')
-    @Roles(UserRole.ADMIN)
-    @UseGuards(RolesGuard)
     async deleteForm(@Param('id') formId: number, @GetUser() user: User): Promise<void> {
         return await this.formsService.deleteForm(formId, user);
     }
